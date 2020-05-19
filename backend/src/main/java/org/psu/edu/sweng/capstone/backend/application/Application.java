7f3f731e9107150054c02b4780b8c05b8b8b4829
unknown <Matt Boyer>
2020-05-18 20:45:13 -0400

    Fixed odd spacing issue

--- backend/src/main/java/org/psu/edu/sweng/capstone/backend/application/Application.java
+++ backend/src/main/java/org/psu/edu/sweng/capstone/backend/application/Application.java
@@ -8,6 +8,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "org.psu.edu.sweng.capstone.backend")
 public class Application {
 	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
