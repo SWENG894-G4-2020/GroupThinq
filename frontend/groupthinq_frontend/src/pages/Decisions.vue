f47f721b14b226cfcf63aa80072680a4e4b40810
Jarrod Brown <jchbrown@gmail.com>
2020-05-29 21:24:59 -0400

    Added dynamic action buttons and details pulldown to decision card.

--- frontend/groupthinq_frontend/src/pages/Decisions.vue
+++ frontend/groupthinq_frontend/src/pages/Decisions.vue
@@ -25,20 +25,26 @@ export default {
       decisionList: [
         {
           UUID: '12341234-3245sdfg1345',
-          Title: 'Decision 1',
+          Title: 'Main Decision Title',
           // eslint-disable-next-line no-multi-str
-          Description: 'Dolorem numquam nor nesciunt and adipisci, explicabo ratione. \
+          Description: 'Decision description. Dolorem numquam nor nesciunt and adipisci, explicabo ratione. \
                       Architecto aut. Ab esse ad. Esse. Accusantium quo. Id rem dolorem nor \
                       sequi dolor yet adipisci. Quam do et anim dolorem. Dolore officia labore \
                       anim pariatur architecto, cillum. Aut anim so do. Vel. Si nostrud quia, \
                       for iure so lorem. Veritatis nostrum yet nisi yet ullamco.',
-          Deadline: '01 September 2020 09:26',
-          Owner: 'username',
+          VoteDeadline: new Date('2020-09-01T09:26:00-04:00'),
+          NominationDeadline: 'Disabled for this decision.',
+          Owner: 'MBoyer',
           Users: [
             'JDoe',
             'PMcCartney',
             'FMiller'
           ],
+          Status: {
+            vote: true,
+            nominate: false,
+            results: false
+          },
           Recurring: false,
           Recurrance: 'None'
         }
