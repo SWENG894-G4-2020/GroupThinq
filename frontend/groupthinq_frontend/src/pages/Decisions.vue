06090f2510d094f5613960c75a26b6cd74cf258e
Jarrod Brown <jchbrown@gmail.com>
2020-06-09 20:42:58 -0400

    Added initial prototype create Decision button and create Decision modal.
    Also reformatted HTML in the AccountInfoCard template.

--- frontend/groupthinq_frontend/src/pages/Decisions.vue
+++ frontend/groupthinq_frontend/src/pages/Decisions.vue
@@ -1,13 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class='col'>
-    <DecisionCard
-      v-for="decision in decisionList"
-      :key="decision.UUID"
-      v-bind="decision"
-    />
-    </div>
-  </q-page>
+    <q-page padding class="column items-center justify-center">
+      <q-btn color="green-8" label="New Decision" icon-right="add"  class="self-end q-mr-xl" @click="createDecision()"/>
+      <DecisionCard
+        v-for="decision in decisionList"
+        :key="decision.UUID"
+        v-bind="decision"
+      />
+      <q-dialog v-model="createDecisionDialog" persistent>
+        <q-card>
+          <q-card-section class='column items-center'>
+            <div class="text-h5 q-pa-md"> Create A New Decision </div>
+            <q-input filled class="q-mb-md" style="width: 100%" v-model="newDecision.name" label="Title" />
+            <q-input filled class="q-my-md" style="width: 100%" v-model="newDecision.expiration" mask="date" :rules="['date']" label="Decision Expiration">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
+                    <q-date v-model="newDecision.expiration" @input="() => $refs.qDateProxy.hide()" />
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </q-card-section>
+          <q-card-actions align="right">
+            <q-btn label="cancel" @click="onCancel()" />
+            <q-btn color="green-8" @click="onCreate()" label="Create Decision" />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </q-page>
 </template>
 
 <script>
@@ -22,6 +42,7 @@ export default {
 
   data () {
     return {
+      createDecisionDialog: true,
       decisionList: [
         {
           UUID: '12341234-3245sdfg1345',
@@ -48,7 +69,25 @@ export default {
           Recurring: false,
           Recurrance: 'None'
         }
-      ]
+      ],
+      newDecision: {
+        Name: '',
+        expiration: ''
+      }
+    }
+  },
+
+  methods: {
+    createDecision () {
+      this.createDecisionDialog = true
+    },
+
+    onCancel () {
+      this.createDecisionDialog = false
+    },
+
+    onCreate () {
+
     }
   }
 }
