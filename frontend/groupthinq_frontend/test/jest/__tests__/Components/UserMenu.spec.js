7703dc5b7fb1154b75e37c36c8d8e2849bf8e251
Jarrod Brown <jchbrown@gmail.com>
2020-06-14 11:51:00 -0400

    Cleaned up lots of extraneous comments from the original example.

--- frontend/groupthinq_frontend/test/jest/__tests__/Components/UserMenu.spec.js
+++ frontend/groupthinq_frontend/test/jest/__tests__/Components/UserMenu.spec.js
@@ -53,39 +53,3 @@ describe('User menu tests', () => {
     expect(vm.$route.path).toBe('/')
   })
 })
-
-  /*
-
-  it('passes the sanity check and creates a wrapper', () => {
-    expect(wrapper.isVueInstance()).toBe(true)
-  })
-
-  it('has a created hook', () => {
-    expect(typeof vm.increment).toBe('function')
-  })
-
-  it('accesses the shallowMount', () => {
-    expect(vm.$el.textContent).toContain('rocket muffin')
-    expect(wrapper.text()).toContain('rocket muffin') // easier
-    expect(wrapper.find('p').text()).toContain('rocket muffin')
-  })
-
-  it('sets the correct default data', () => {
-    expect(typeof vm.counter).toBe('number')
-    const defaultData2 = QBUTTON.data()
-    expect(defaultData2.counter).toBe(0)
-  })
-
-  it('correctly updates data when button is pressed', () => {
-    const button = wrapper.find('button')
-    button.trigger('click')
-    expect(vm.counter).toBe(1)
-  })
-
-  it('formats a date without throwing exception', () => {
-    // test will automatically fail if an exception is thrown
-    // MMMM and MMM require that a language is 'installed' in Quasar
-    let formattedString = date.formatDate(Date.now(), 'YYYY MMMM MMM DD')
-    console.log('formattedString', formattedString)
-  })
-  */
