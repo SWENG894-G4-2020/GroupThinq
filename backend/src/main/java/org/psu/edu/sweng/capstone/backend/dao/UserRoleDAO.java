1c3af0b959df2b7009366dc107983d25e3df0a82
Matt Boyer <mboyer87@gmail.com>
2020-07-18 14:04:32 -0400

    Ballot_Options_Votes: Removed unused code and warnings

--- backend/src/main/java/org/psu/edu/sweng/capstone/backend/dao/UserRoleDAO.java
+++ backend/src/main/java/org/psu/edu/sweng/capstone/backend/dao/UserRoleDAO.java
@@ -1,13 +1,8 @@
 package org.psu.edu.sweng.capstone.backend.dao;
 
-import java.util.ArrayList;
-
-import org.psu.edu.sweng.capstone.backend.model.User;
 import org.psu.edu.sweng.capstone.backend.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRoleDAO extends JpaRepository<UserRole, Long> {
-	ArrayList<UserRole> findAllByUser(User user);
-}
+public interface UserRoleDAO extends JpaRepository<UserRole, Long> {}
