7acea5c4166a8921ca6db96e4c1256bb7d477035
Sean Davis <sean@seandavis.dev>
2020-08-02 15:21:47 -0500

    Added user search bar

--- frontend/groupthinq_frontend/src/pages/Users.vue
+++ frontend/groupthinq_frontend/src/pages/Users.vue
@@ -1,11 +1,37 @@
 <template>
-  <q-page class="flex flex-center" v-if='isLoaded'>
-    <UserCard
-      v-for="user in users"
+  <q-page class="q-pa-md">
+    <div v-if='isLoaded' class="row q-gutter-md q-pr-md">
+      <div class="col-xs-12">
+          <q-input
+            v-model="search"
+            debounce="0"
+            filled
+            placeholder="Search"
+            >
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+      </div>
+      <UserCard
+      v-for="user in filteredUsers"
       :key="user.userName"
       v-bind="user"
-      class='q-pa-md q-ma-lg'
       />
+    </div>
+    <div v-else-if="!isError">
+      <div class="text-h5 text-primary">Loading...
+        <q-spinner-hourglass color="primary" size="2em"/>
+      </div>
+    </div>
+    <div v-else>
+      <div class="text-h5 text-negative self-center">
+        Something went wrong.
+        <q-icon name="warning" />
+      </div>
+      <div class="text-h7 text-negative self-center">Please refresh the page.
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -18,15 +44,12 @@ export default {
     UserCard
   },
 
-  methods: {
-    async loadUserData () {
-      try {
-        const response = await this.$axios.get(`${process.env.BACKEND_URL}/users`)
-        this.users = response.data.data
-        this.isLoaded = true
-      } catch (error) {
-        console.log(error)
-      }
+  data () {
+    return {
+      isLoaded: false,
+      isError: false,
+      users: [],
+      search: ''
     }
   },
 
@@ -34,10 +57,27 @@ export default {
     this.loadUserData()
   },
 
-  data () {
-    return {
-      isLoaded: false,
-      users: []
+  computed: {
+    filteredUsers: function () {
+      return this.users.filter(user =>
+        user.userName.toLowerCase().includes(this.search.toLowerCase()) ||
+        user.firstName.toLowerCase().includes(this.search.toLowerCase()) ||
+        user.lastName.toLowerCase().includes(this.search.toLowerCase())
+      )
+    }
+  },
+
+  methods: {
+    async loadUserData () {
+      try {
+        const response = await this.$axios.get(`${process.env.BACKEND_URL}/users`)
+        this.users = response.data.data
+        console.log(this.users)
+        this.isLoaded = true
+      } catch (error) {
+        console.log(error)
+        this.isError = true
+      }
     }
   }
 }
