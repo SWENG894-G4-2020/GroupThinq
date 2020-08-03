3e08c243a608f685e065594c44f298835edd2f09
unknown <Matt Boyer>
2020-08-03 06:48:35 -0400

    CAP-210: Fixed description not being added to Decision

--- backend/src/main/java/org/psu/edu/sweng/capstone/backend/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/org/psu/edu/sweng/capstone/backend/security/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package org.psu.edu.sweng.capstone.backend.security;
 
 import org.psu.edu.sweng.capstone.backend.dao.UserDAO;
-import org.psu.edu.sweng.capstone.backend.model.Role;
 import org.psu.edu.sweng.capstone.backend.model.User;
 import org.psu.edu.sweng.capstone.backend.model.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import static java.util.Collections.emptyList;
 
 import java.util.HashSet;
 import java.util.Optional;
